43f179002fc6d969508175894660bd311d39dd70
Moazam Hussain <moazam694@gmail.com>
2022-03-29 15:55:36 -0700

    added CSS

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 
 function App () {
   const [result, setResult] = useState("");
@@ -12,24 +12,24 @@ function App () {
         </form>
 
         <div className='keypad'>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+          <button>Clear</button>
+          <button>C</button>
+          <button>&divide;</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>&times;</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>&ndash;</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
         </div>
       </div>
     </>
